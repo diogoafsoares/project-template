47f9517ac218b3a7974e4946642d8b78715a470c
diogoafsoares <diogoafsoares@hotmail.com>
2015-12-23 16:51:06 +0000

    V1.1

--- app/ProjectoAna/cwApp.js
+++ app/ProjectoAna/cwApp.js
@@ -1,20 +1,40 @@
 var app = angular.module('cwApp', []);
-app.controller('cwCtrl', function($scope) {
-                                                            $scope.default_user = { firstname: "Patricia", lastname: "Ruivo"};
-                                                            $scope.reset_form = function() {
-                                                                           $scope.user = angular.copy($scope.default_user);
-                                                            };
-                                                            $scope.reset_form();
-                                                            
-                                                            $scope.fullname = function() {
-                                                                           return $scope.user.firstname + " " + $scope.user.lastname;
-                                                            };
-                                                            $scope.count = 0;
-                                             });
+
+app.controller('defaultCtrl', function($scope, $http){
+	$http.get("http://127.0.0.1:8090/cwData.json").then(function(response) {
+               $scope.courses = response.data.courses;
+               });
+			   $http.get("http://127.0.0.1:8090/patients.json").then(function(response){
+			$scope.patients = response.data;
+		});
+		$http.get("http://127.0.0.1:8090/requests.json").then(function(response){
+			$scope.requests = response.data;
+		});
+		
+		$scope.first=true;
+		$scope.second=false;
+		$scope.third=false;
+		
+		$scope.gotosecond = function (){
+			$scope.first=false;
+		$scope.third=false;
+		$scope.second=true;
+		}
+		$scope.gotothird = function(p){
+			$scope.patient = p.name;
+			$scope.policy = p.policy_number;
+			$scope.policytype = p.policy_type;
+			$scope.patid = p.patID;
+			$scope.first=false;
+		$scope.second=false;
+		$scope.third=true;
+		}
+			   
+})
                                              
 app.controller('coursesCtrl', function($scope, $http){
                $http.get("http://127.0.0.1:8090/cwData.json").then(function(response) {
-                                             $scope.courses = response.data.courses;
+               $scope.courses = response.data.courses;
                });
 });
 
